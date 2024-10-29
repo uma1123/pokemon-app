e49718c6e97f0854d4c9f9a421cbcfc2002aa8c7
uma1123 <yuma1596@gmail.com>
2024-10-29 15:43:25 +0900

    ホバー(拡大)を追加

--- src/card.tsx
+++ src/card.tsx
@@ -9,15 +9,15 @@ interface CardProps {
 export default function Card({ id, name, image, type, className }: CardProps) {
   return (
     <div
-      className={` ${className} flex flex-col items-center justify-center py-6 m-1 border-solid border-gray-300 rounded-md min-w-40 text-center shadow-md shadow-black delay-300 relative cursor-pointer`}
+      className={` ${className} flex flex-col items-center justify-center py-6 m-1 border-solid border-gray-300 rounded-md min-w-40 text-center shadow-md shadow-black relative cursor-pointer transform transition-transform duration-300 hover:scale-110`}
     >
-      <div className="">
+      <div className="rounded-2xl py-2 px-1 bg-white bg-opacity-30">
         <small>{id}</small>
       </div>
       <img src={image} alt={name} className="w-[120px] h-[120px]" />
       <div className="">
         <h4>{name}</h4>
-        <h5>{type}</h5>
+        <h3 className="mb-1">{type}</h3>
       </div>
     </div>
   );
